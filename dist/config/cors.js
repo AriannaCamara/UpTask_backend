609dfc87b9b3151efedc573cd4edbc4f8d45bade
Arianna Camara <ariannaisabel795@gmail.com>
2025-12-10 18:28:41 +0100

    Fix: cambios para deploy

--- dist/config/cors.js
+++ dist/config/cors.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsConfig = void 0;
 exports.corsConfig = {
     origin: function (origin, callback) {
-        const whitelist = [process.env.FRONTED_URL];
+        const whitelist = [process.env.FRONTEND_URL];
         if (process.argv[2] === '--api') {
             whitelist.push(undefined);
         }
